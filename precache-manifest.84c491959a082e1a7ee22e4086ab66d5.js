090a09058b5704d68c6ac3c7f28302e209e9daba
socopoko <59167401+socopoko@users.noreply.github.com>
2020-06-29 15:30:00 +0530

    Updates

---
+++ precache-manifest.84c491959a082e1a7ee22e4086ab66d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a465c55016fa69892bdcd8eb9fe192af",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "162f43d5af4bda97ccf5",
+    "url": "/monsters-rolodex/static/css/main.9f3231ed.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "162f43d5af4bda97ccf5",
+    "url": "/monsters-rolodex/static/js/main.66dcaa8e.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
